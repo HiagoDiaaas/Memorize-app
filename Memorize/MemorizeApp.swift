217e2388d5a6fc73dcf03e2ca069c50dfb405a72
Hiago Santos Martins Dias <hiagosmd@hotmail.com>
2022-05-03 06:33:17 -0300

    project created

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hiago Santos Martins Dias on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
